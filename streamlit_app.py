e70eaa5f75890efe75b4489001c7e4183726fb58
Jordan <61518071+joelazhunter@users.noreply.github.com>
2024-02-05 10:34:17 +0100

    Update streamlit_app.py
    some more corrections in the if condition

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,7 @@ streamlit.dataframe(my_data_row)
 
 #allow user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add:')
-streamlit.write('Thanks for adding ', add_my_fruit)
+
 if(len(add_my_fruit)>0):
- my_cur.execute("insert into fruit_load_list values (" + add_my_fruit + ")")
+ my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
+ streamlit.write('Thanks for adding ', add_my_fruit)
