5050a96b551fafc7df94782c48cd90a9a95db1d0
Jordan <61518071+joelazhunter@users.noreply.github.com>
2024-02-05 10:29:28 +0100

    Update streamlit_app.py
    corrected errpr on line 47 missing '

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,6 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 #allow user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add:)
+add_my_fruit = streamlit.text_input('What fruit would you like to add:')
 streamlit.write('Thanks for adding ', add_my_fruit)
 my_cur.execute("insert into fruit_load_list values (" + add_my_fruit + ")")
